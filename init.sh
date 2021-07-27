33e29f85b116c5f1aade9f828e12c5a6cd7c236b
Bruno Carpio <bcarpio181@outlook.es>
2021-07-27 12:33:01 -0500

    init /usr/sbin/mosquitto change

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 #mosquitto -c mosquitto.conf &>/dev/null &
 
 ./key_authority.py &
-mosquitto -c mosquitto.conf &
+/usr/sbin/mosquitto -c mosquitto.conf &
 
 printf "Key Authority server inicio\n"
 printf "Mosquitto server inicio\n"
